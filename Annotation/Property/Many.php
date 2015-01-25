05a9aeada5e8fd8a3a9f8abfa7975ac18a892b02
Roman Pavlík <zzromanzz@gmail.com>
2015-01-25 10:06:25 +0100

    Fixed escaped sequences

--- Annotation/Property/Many.php
+++ Annotation/Property/Many.php
@@ -86,7 +86,7 @@ class Many extends \obo\Annotation\Base\Property {
             $relationship->sortVia = $this->sortVia = $values["sortVia"];
         }
 
-        $this->propertyInformation->dataType = new \obo\DataType\Object($this->propertyInformation, "\obo\Relationships\EntitiesCollection");
+        $this->propertyInformation->dataType = new \obo\DataType\Object($this->propertyInformation, "\\obo\\Relationships\\EntitiesCollection");
     }
 
     /**
