81191392b5014ca5969fb7711b0708a0948c373f
Adam Suba <mail@adamsuba.cz>
2016-08-04 12:10:48 +0200

    Changed version number

--- src/obo.php
+++ src/obo.php
@@ -13,7 +13,7 @@ namespace obo;
 class obo extends \obo\Object {
 
     const _NAME = "obo";
-    const _VERSION = "v0.12-dev";
+    const _VERSION = "v0.12.0";
     const _VERSION_ID = 1200;
     const _LICENCE = "Apache License, Version 2.0";
     const _WWW = "http://www.obophp.org/";
