071b8dbb22202e2d3d39df9ba8464265ddc2a1f5
oboPHP <info@obophp.org>
2017-06-21 15:31:10 +0200

    Changed version number

--- src/obo.php
+++ src/obo.php
@@ -13,8 +13,8 @@ namespace obo;
 class obo extends \obo\Object {
 
     const _NAME = "obo";
-    const _VERSION = "v0.16.0";
-    const _VERSION_ID = 1600;
+    const _VERSION = "v0.16.1";
+    const _VERSION_ID = 1601;
     const _LICENCE = "Apache License, Version 2.0";
     const _WWW = "http://www.obophp.org/";
 
