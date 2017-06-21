31a4bd0b4de931e08948f1c98d65b09439780537
Adam Suba <mail@adamsuba.cz>
2017-06-21 11:49:06 +0200

    Refactored test structure

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,7 +1,5 @@
 <?php
 
-// The Nette Tester command-line runner can be
-// invoked through the command: ../vendor/bin/tester .
 if (@!include __DIR__ . "/../vendor/autoload.php") {
     echo "Install Nette Tester using `composer update`";
     exit(1);
@@ -11,10 +9,10 @@ Tester\Environment::setup();
 date_default_timezone_set("Europe/Prague");
 
 \obo\obo::$developerMode = true;
-\obo\obo::setCache(new obo\Tests\Assets\Cache(__DIR__ . DIRECTORY_SEPARATOR . "temp"));
-\obo\obo::setTempDir(__DIR__ . DIRECTORY_SEPARATOR . "temp");
+\obo\obo::setCache(new obo\Tests\Assets\Cache(__DIR__ . "/temp"));
+\obo\obo::setTempDir(__DIR__ . "/temp");
 \obo\obo::addModelsDirs([
-   __DIR__ . DIRECTORY_SEPARATOR . "__assets" . DIRECTORY_SEPARATOR . "Entities",
+   __DIR__ . "/",
 ]);
 
 \obo\obo::setDefaultDataStorage(\obo\Tests\Assets\DefaultDataStorageFactory::createDataStorage());
