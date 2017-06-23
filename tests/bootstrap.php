903505cfdd094eacf181b3c78dc5621e5ba434c2
Adam Suba <mail@adamsuba.cz>
2017-06-23 15:12:34 +0200

    Added debugging tool to tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -8,8 +8,31 @@ if (@!include __DIR__ . "/../vendor/autoload.php") {
 Tester\Environment::setup();
 date_default_timezone_set("Europe/Prague");
 
+\Tracy\Debugger::$logDirectory = __DIR__ . "/log";
+
+$_prevEx = set_exception_handler(function($e) use (& $_prevEx) {
+    if (!$e instanceof \Tester\AssertException) \Tracy\Debugger::log($e, \Tracy\ILogger::EXCEPTION);
+
+    if ($_prevEx !== null) {
+        $_prevEx($e);
+    }
+});
+
+$_prevEr = set_error_handler(function($severity, $message, $file, $line) use (& $_prevEr) {
+    if (error_reporting() === 0) return false;
+    $e = new \ErrorException($message, 0, $severity, $file, $line);
+
+    \Tracy\Debugger::log($e, \Tracy\ILogger::EXCEPTION);
+
+    if ($_prevEr === null) {
+        return false;
+    } else {
+        call_user_func_array($_prevEr, func_get_args());
+    }
+});
+
 \obo\obo::$developerMode = true;
-\obo\obo::setCache(new obo\Tests\Assets\Cache(__DIR__ . "/temp"));
+\obo\obo::setCache(new \obo\Tests\Assets\Cache(__DIR__ . "/temp"));
 \obo\obo::setTempDir(__DIR__ . "/temp");
 \obo\obo::addModelsDirs([
    __DIR__ . "/",
